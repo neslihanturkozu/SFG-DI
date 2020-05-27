7a9172e4930153bf1e5604fe2990c0bf1bca61a3
Neslihan Türközü <neslihanturkozu@gmail.com>
2020-05-27 16:13:26 +0300

    Adding Spring DI examples

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
+
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
